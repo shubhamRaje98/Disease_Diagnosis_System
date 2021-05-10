7f4d257f29119fad8b0666a3b6478b03b3bf33b9
Shubham Shejaval <59889321+shubhamRaje98@users.noreply.github.com>
2021-05-10 21:10:36 +0530

    Update Result_dia.py

--- Result_dia.py
+++ Result_dia.py
@@ -3,13 +3,15 @@ from tkinter import ttk
 from tkinter.ttk import *
 from PIL import ImageTk, Image
 from tkinter import messagebox
+import smtplib
 
 class GUI_result:
 
-	def __init__(self, disease_name="", fatality_risk=0, risk_fac=0):
+	def __init__(self, disease_name="", fatality_risk=0, risk_fac=0, res=""):
 		Result_root = Tk()
 		Result_root.title("Diagnosis Result")
-		Result_root.geometry("1200x750")
+		cent = "800x660+300+40"
+		Result_root.geometry(cent)
 
 		#Setting up canvas:
 		Result_my_canvas = Canvas(Result_root)
@@ -22,7 +24,7 @@ class GUI_result:
 		Result_my_canvas.create_image(0, 0, image=Result_bg, anchor="nw")
 
 		# Creating title text:
-		Result_my_canvas.create_text(650, 40, text="Diasgnosis result : ", font=("Times", 20), fill="white")
+		Result_my_canvas.create_text(410, 40, text="Diasgnosis result : ", font=("Times", 20), fill="white")
 		
 
 		# line 1:
@@ -31,13 +33,20 @@ class GUI_result:
 
 		# line 2:
 		Result_my_canvas.create_text(110, 200, text="Risk factor : ", font=("helvetica", 15), fill="black")
-		Result_my_canvas.create_text(240, 200, text=str(risk_fac) , font=("helvetica", 15), fill="black")
+		Result_my_canvas.create_text(240, 200, text=str(risk_fac) + "%", font=("helvetica", 15), fill="black")
 
 		# line 3:
 		Result_my_canvas.create_text(120, 300, text="Risk of fatality : ", font=("helvetica", 15), fill="black")
-		Result_my_canvas.create_text(240, 300, text=str(fatality_risk) , font=("helvetica", 15), fill="black")
+		Result_my_canvas.create_text(240, 300, text=str(fatality_risk) + "%", font=("helvetica", 15), fill="black")
 
+		server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+		server.login("diseasediagnosissystem@gmail.com", "*Shubh@m9820632974")
+		mailContent = f'\nHello sir, your dianosis result is : \n\n Diagnosed Disease : {disease_name} \n Risk Factor : {risk_fac}% \n fatality_risk : {fatality_risk}% \n\n We advice you to consult a doctor' # Here add \n in start of string to make it work
+		print(mailContent)
+		print(res)
+		server.sendmail("diseasediagnosissystem@gmail.com", res, mailContent)
 
+		server.quit()
 
 		Result_root.mainloop()
 
